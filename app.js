3e4ef59fdc904f2e5679b5c21278c5fee20d5c61
Matthew Tang <Matthew Tang>
2017-05-17 09:14:13 -0700

    add attachment attempt 3

--- app.js
+++ app.js
@@ -22,26 +22,15 @@ app.post('/hello', function (req, res, next) {
 	"mrkdwn": true
 	"attachments": [
         {
-            "fallback": "Xkcd comic.",
+            "fallback": "Xkcd comic. - https://imgs.xkcd.com/comics/proofs.png",
             "color": "#36a64f",
             "pretext": "Here's a random comic",
             "author_name": "Xkcdbot",
-            "author_link": "google.com",
             "author_icon": "https://imgs.xkcd.com/comics/proofs.png",
             "title": "XKCD Comic",
-            "title_link": "https://api.slack.com/",
+            "title_link": "https://imgs.xkcd.com/comics/proofs.png",
             "text": "Here's your comic",
-            "fields": [
-                {
-                    "title": "Proofs",
-                    "short": false
-                }
-            ],
-            "image_url": "https://imgs.xkcd.com/comics/proofs.png",
-            "thumb_url": "http://example.com/path/to/thumb.png",
-            "footer": "Xkcd comics",
-            "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
-            "ts": 123456789
+            "image_url": "https://imgs.xkcd.com/comics/proofs.png"
         }
     ]
   };
