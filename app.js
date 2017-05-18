2f173f2d9ad796e6b3d19fbedd6cd76d14a6cd12
matthew29tang <matthew29tang@gmail.com>
2017-05-18 16:23:33 -0700

    fix lnk var

--- app.js
+++ app.js
@@ -19,10 +19,11 @@ app.listen(port, function () {
 app.post('/comic', function (req, res, next) {
   var userName = req.body.user_name;
   var randomID=Math.floor(Math.random() * 1830);
+  var lnk=0
   fs.readFile('comics.txt', 'utf8', function(err, data) {  
 	if (err) throw err;
 	var data=data.split('\n');		
-	var lnk=data[randomID]
+	lnk=data[randomID]
 	});
   var botPayload = {
     //"text" : '*Hello ' + userName + ', welcome!*',
@@ -32,7 +33,7 @@ app.post('/comic', function (req, res, next) {
         {
             "fallback": "Xkcd comic. - https://imgs.xkcd.com/comics/proofs.png",
             "color": "#36a64f",
-            "pretext": "Here's a random comic"+lnk,
+            "pretext": lnk+"Here's a random comic",
             "title": "XKCD Comic",
             "title_link": "https://imgs.xkcd.com/comics/proofs.png",
             "text": "Here's your comic",
