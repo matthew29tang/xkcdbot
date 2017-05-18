6567d848debb1a48b1632f4cdb510f90f95de65d
matthew29tang <matthew29tang@gmail.com>
2017-05-17 21:16:57 -0700

    add random attempt 1

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ var bodyParser = require('body-parser');
  
 var app = express();
 var port = process.env.PORT || 1337;
+
+function getRandomLine(filename){
+  fs.readFile(filename, function(err, data){
+    if(err) throw err;
+    var lines = data.split('\n');
+    lines[Math.floor(Math.random()*lines.length)];
+ })
+}
  
 // body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,6 +24,13 @@ app.listen(port, function () {
 
 app.post('/comic', function (req, res, next) {
   var userName = req.body.user_name;
+  var randomID=Math.floor((Math.random() * 1830) + 1);
+  fs.readFile("comics.txt", function(err, data){
+	  if(err) throw err;
+	  var lines=data.split('\n');
+	  var lnk=lines[randomID]
+	  
+  }
   var botPayload = {
     //"text" : '*Hello ' + userName + ', welcome! I\'ll be your guide.*',
 	//"username": "Heibot",
@@ -24,7 +39,7 @@ app.post('/comic', function (req, res, next) {
         {
             "fallback": "Xkcd comic. - https://imgs.xkcd.com/comics/proofs.png",
             "color": "#36a64f",
-            "pretext": "Here's a random comic",
+            "pretext": lnk,#"Here's a random comic",
             "title": "XKCD Comic",
             "title_link": "https://imgs.xkcd.com/comics/proofs.png",
             "text": "Here's your comic",
