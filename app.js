1d7e3fea9b6e1557db141d293e1d95522a66fb55
matthew29tang <matthew29tang@gmail.com>
2017-05-17 23:50:34 -0700

    rename to filesystem

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var math = require('mathjs');
-var fs = require('fs');
+var fs = require('file-system');
  
 var app = express();
 var port = process.env.PORT || 1337;
