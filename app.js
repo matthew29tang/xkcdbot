86ece980593783be61843a7804bd0a571efaf584
matthew29tang <matthew29tang@gmail.com>
2017-05-18 17:02:06 -0700

    fix syntax2

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.post('/comic', function (req, res, next) {
         {
             "fallback": "Xkcd comic. - https://imgs.xkcd.com/comics/proofs.png",
             "color": "#36a64f",
-            "pretext": lnk+"Here's a random comic"+data,
+            "pretext": lnk+"Here's a random comic"+d,
             "title": "XKCD Comic",
             "title_link": "https://imgs.xkcd.com/comics/proofs.png",
             "text": "Here's your comic",
