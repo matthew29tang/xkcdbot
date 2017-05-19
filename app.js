66a35029e161f5f4cbe8b484aa2f326edd4f17f2
matthew29tang <matthew29tang@gmail.com>
2017-05-18 17:50:09 -0700

    testing

--- app.js
+++ app.js
@@ -22,12 +22,9 @@ app.post('/comic', function (req, res, next) {
   var userName = req.body.user_name;
   var randomID=Math.floor(Math.random() * 1830);
   var lnk=0;
-  var d="hii";
-  fs.readFile(path.join(process.cwd(),'comics.txt'), function(err, data) {  
-	if (err) return res.status(200).json('"text":"fail"');
-	var d=data.split('\n');		
-	var lnk=d[randomID]
-	});
+  var d="h i j";
+  var inf=d.split(' ');		
+  var lnk=inf[1]
   var botPayload = {
     //"text" : '*Hello ' + userName + ', welcome!*',
 	//"username": "Heibot",
@@ -36,8 +33,8 @@ app.post('/comic', function (req, res, next) {
         {
             "fallback": "Xkcd comic. - https://imgs.xkcd.com/comics/proofs.png",
             "color": "#36a64f",
-            "pretext": lnk+"Here's a random comic"+d,
-            "title": process.cwd(),
+            "pretext": lnk+"Here's a random comic",
+            "title": "XKCD COMIC",
             "title_link": "https://imgs.xkcd.com/comics/proofs.png",
             "text": "Here's your comic",
             "image_url": "https://imgs.xkcd.com/comics/proofs.png"
